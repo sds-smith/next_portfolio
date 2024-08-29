76d38f1adfb1641f5b7b83a876d98b01299e576f
Shawn Smith <sds.smith24@gmail.com>
2024-08-29 11:50:50 -0400

    sections

--- src/app/about/sections.js
+++ src/app/about/sections.js
@@ -7,15 +7,16 @@ const copy1 = [
 
 const copy2 = [
     ["For the past two years I have worked as part of a distributed team for a small Federal contractor. In that time I've had the opportunity to work in an Agile project environment and on three distinct projects.",
-    "Through code review and pair programming, I've had the opportunity to help fellow developers on their journey to become better developers while also learning from them along my own journey. I've had the opportunity to make technical and architectural decisions, prepare technical documentation, establish and maintain production, stage, and test environments as well as CI/CD pipeline.",
+    "Through code review and pair programming, I've had the opportunity to help fellow developers on their journey to become better developers while also learning from them along my own journey. I've had the opportunity to make technical and architectural decisions, prepare technical documentation, and establish and maintain a CI/CD pipeline including production, stage, and test environments.",
     "In my current role, I am creating from scratch a proof-of-concept SPFx app with React frontend intended to replace a legacy application which was written in vanilla JavaScript with jQuery."].join(' '),
-    ["Three years ago, I ventured into the gig economy as a freelance developer. The hallmark of this effort was the delivery of an MVP to a client which is currently hosted on the public internet as a subscription-based productivity application.",
+    ["Three years ago, I ventured into the gig economy as a freelance developer. The hallmark of this effort was the delivery of an MVP for an application which is currently hosted on the public internet as a subscription-based productivity app.",
     "Working hand-in-hand with the client, based on his Figma design for a kanban-style board for managing one-on-one discussion topics, I delivered a MERN app with Material UI components which runs in a Docker container."].join(' ')
 ]
 
 const copy3 = [
-  "Software development is Act II of my career journey. Act I was a dynamic and fulfilling career in the Food and Hospitality industry. Serving in leadership roles of progressively increasing scopes of influence and responsibility, I spent my final ten years overseeing an organization of six locations, 26 managers, and more than 300 front-line team members.",
-  "This was a people role in a people business. Software, too, is a people business. Software is for people -- the people who use it, and the people who develop and maintain it. I believe the skills and qualities of character that make a successful leader in the restaurant world are infinitely transferrable; skills like partnering, mentoring, and listening; and qualities like humility, vision, and empathy."
+  "Software development is Act II of my career journey.", 
+  "Act I was a dynamic and fulfilling career in the Food and Hospitality industry. Serving in leadership roles of progressively increasing scopes of influence and responsibility, I spent my final ten years overseeing an organization of six locations, 26 managers, and more than 300 front-line team members. This was a people role in a people business. ",
+  "Software, too, is a people business. Software is for people -- the people who use it, and the people who develop and maintain it. I believe the skills and qualities of character that make a successful leader in the restaurant world are infinitely transferrable; skills like partnering, mentoring, and listening; and qualities like humility, vision, and empathy."
 ]
 
 export const sections = [
