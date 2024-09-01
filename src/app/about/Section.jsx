2091ce360745f12989a1e325699ff66b63dbd97a
Shawn Smith <sds.smith24@gmail.com>
2024-09-01 10:55:11 -0400

    comittery

--- src/app/about/Section.jsx
+++ src/app/about/Section.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
-import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import styles from "../page.module.css";
 
 export default function Section({section}) {
   return (
-    <Container component='section' className={styles.section} >
-     <Typography className={styles.section_title} variant='h5'>{section.title}</Typography>
-     {section.copy.map(p => <Typography className={styles.section_paragraph} key={p}>{p}</Typography>)}
-    </Container>
+    <Grid container item xs={12} spacing={3} component='section' className={styles.section} >
+      <Grid item xs={12}>
+        <Typography className={styles.section_title} variant='h5'>{section.title}</Typography>
+      </Grid>
+     {section.copy.map(p => (
+      <Grid item xs={12} key={p}>
+        <Typography className={styles.section_paragraph}>{p}</Typography>
+      </Grid>
+    ))}
+    </Grid>
   )
 }
