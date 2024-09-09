7ab0ab164c26b05ff9b17ddac410a6661ebadc67
Shawn Smith <sds.smith24@gmail.com>
2024-09-09 11:19:18 -0400

    changes

--- src/app/portfolio/page.js
+++ src/app/portfolio/page.js
@@ -15,17 +15,18 @@ export default function Portfolio() {
       <Signature page='portfolio' />
       <Grid container item xs={12} md={9} p={{xs: '0 3rem', md: '0 6rem'}} mt={{md: '4rem'}}>
         {projects.map(p => (
-          <Grid container key={p.id} className={styles.card} >
-            <Grid item xs={12} md={3}>
-            <Image src={p.background_image} alt={p.id} style={{width: '200px', height: 'auto'}}/>
-            </Grid>
-            <Grid item xs={12} md={9} >
-            <Typography className={styles.card_h2} variant='h5'>{p.name}</Typography>
-            <Typography className={styles.card_p}>{p.description}</Typography>
-            <Link className={styles.card_link} href={p.repo_link} target='_blank' rel='no-referrer'>{p.action}<span>-&gt;</span></Link>
-
-            </Grid>
-          </Grid >
+          <a key={p.id} className={styles.card_link} href={p.repo_link} target='_blank' rel='no-referrer'>
+            <Grid container className={styles.card} >
+              <Grid item xs={12} md={3}>
+                <Image src={p.background_image} alt={p.id} style={{width: '200px', height: 'auto'}}/>
+              </Grid>
+              <Grid item xs={12} md={9} >
+                <Typography className={styles.card_h2} variant='h5'>{p.name}</Typography>
+                <Typography className={styles.card_p}>{p.description}</Typography>
+                <Typography className={styles.card_pointer} href={p.repo_link} target='_blank' rel='no-referrer'>{p.action}<span>-&gt;</span></Typography>
+              </Grid>
+            </Grid >
+          </a>
         ))}
       </Grid>
     </GridComponentMain>  
