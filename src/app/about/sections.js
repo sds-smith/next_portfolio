ba4f176c716326b362178a0d044b75dfa34fa3a2
Shawn Smith <sds.smith24@gmail.com>
2024-09-18 10:27:05 -0400

    changes

--- src/app/about/sections.js
+++ src/app/about/sections.js
@@ -1,16 +1,16 @@
 
 const copy1 = [
-    "I like to solve tough problems. I want to work on challenging projects that make a difference for others, surrounded by people smarter than me who will challenge me. I love to build things that work, complex things designed with simplicity and elegance.",
-    "I believe in first listening to understand needs, challenges, and perspectives, then formulating a response to meet those needs, overcome the challenges and honor perspectives.", 
-    "Software is foremost about people - the people who use it and the people who develop and maintain it. As a Software Engineer, I put people first, crafting solutions that satisfy their needs with technologies that fit the solution.",
+    "I like to solve tough problems. I want to work on challenging projects that make a difference for others, surrounded by people smarter than me who inspire me. I love to build things that work, complex things designed with simplicity and elegance.",
+    "I believe in first listening to understand needs, perspectives, and obstacles, then formulating a response to meet those needs, overcome the obstacles and honor perspectives.", 
+    "Software is foremost about people - the people who use it and the people who develop and maintain it. As a Software Engineer, I put people first, crafting solutions around their needs, choosing technologies that fit the solution.",
     "I believe it's our job as developers to produce real and measurable results for business owners, while also leaving a codebase for fellow and future developers that is clean, maintainable, and extensible. There is nothing more important to the health of a codebase than clean code/ clean architecture, but it means nothing if it doesn't produce real value for the customer right now.",
     "I am guided by a lifelong curiosity and tireless desire to be better today than yesterday, better tomorrow than today."
 ];
 
 const copy2 = [
-    ["I currently work as part of a distributed team for a small Federal contractor. In my role, I've had the opportunity to work in an Agile project environment and on three distinct projects.",
+    ["In my current role, I work as part of a distributed team for a small Federal contractor. In this role, I've had the opportunity to work in an Agile project environment and on three distinct projects.",
     "Through code review and pair programming, I've had the opportunity to help fellow developers on their journey to become better developers while also learning from them along my own journey. I've had the opportunity to make technical and architectural decisions, prepare technical documentation, and establish and maintain a CI/CD pipeline including production, stage, and test environments.",
-    "In my current role, I am creating from scratch a proof-of-concept SPFx app with React frontend intended to replace a legacy application which was written in vanilla JavaScript with jQuery."].join(' '),
+    "Currently, I am creating from scratch a proof-of-concept SPFx app with React frontend intended to replace a legacy application which was written in vanilla JavaScript with jQuery."].join(' '),
     ["Three years ago, I ventured into the gig economy as a freelance developer. The hallmark of this effort was the delivery of an MVP for an application which is currently hosted on the public internet as a subscription-based productivity app.",
     "Working hand-in-hand with the client, based on his Figma design for a kanban-style board for managing one-on-one discussion topics, I delivered a MERN app with Material UI components which runs in a Docker container."].join(' ')
 ]
