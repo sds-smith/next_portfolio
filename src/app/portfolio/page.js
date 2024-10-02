2a05f5c9bd7112b15a8d3c8f4c4cb8cc9654d50d
Shawn Smith <sds.smith24@gmail.com>
2024-10-02 07:39:35 -0400

    add counterpoint craft kitchen

--- src/app/portfolio/page.js
+++ src/app/portfolio/page.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import Image from 'next/image';
 import Grid from '@mui/material/Grid';
-import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
-import Link from '@mui/material/Link';
 import GridComponentMain from '../GridComponentMain';
 import Signature from '../Signature';
 import { projects } from '../../assets/projects.data';
 import styles from "../page.module.css";
+
+const imageStyle = {
+  true: {height: '200px', width: 'auto', margin: '0 auto'},
+  false: {height: 'auto', width: '200px'}
+}
 
 export default function Portfolio() {
   return (
@@ -17,8 +20,8 @@ export default function Portfolio() {
         {projects.map(p => (
           <a key={p.id} className={styles.card_link} href={p.repo_link} target='_blank' rel='no-referrer'>
             <Grid container className={styles.card} >
-              <Grid item xs={12} md={3}>
-                <Image src={p.background_image} alt={p.id} style={{width: '200px', height: 'auto'}}/>
+              <Grid container item xs={12} md={3}>
+                <Image src={p.background_image} alt={p.id} style={imageStyle[p.image_orientation === 'portrait']}/>
               </Grid>
               <Grid item xs={12} md={9} >
                 <Typography className={styles.card_h2} variant='h5'>{p.name}</Typography>
--- src/assets/projects.data.js
+++ src/assets/projects.data.js
@@ -2,34 +2,23 @@
 import Boss from './media/boss.png';
 import DiscoverBreweries from './media/discover-breweries.png'
 import AssembleTheJams from './media/atj.png';
+import Counterpoint from './media/IMG_3436.jpeg'
 
 export const projects = [
     {   
-        id: '1on1boss-card',
-        name: '1on1Boss',
-        action: 'learn more',
-        heading_title : 'Kanban-style Discussion Board',
-        heading_subtitle : 'Full-stack MERN App',
-        background_image : Boss,
-        video_link : '',
-        live_link : 'http://app.1on1boss.com/',
-        repo_link : 'https://www.1on1boss.com/the-team',
-        readme_link : 'https://www.1on1boss.com',
-        description : 'An MVP for a kanban-style discussion management board. Built on the MERN tech stack, runs in Docker container.'
-    },  
-    {   
-        id: 'discover-breweries-card',
-        name: 'Discover Breweries',
+        id: 'counterpoint-kitchen-card',
+        name: 'Counterpoint Craft Kitchen',
         action: 'view on github',
-        heading_title : 'Brewery Locator',
-        heading_subtitle : 'React static frontend with AWS Lambda',
-        background_image : DiscoverBreweries,
+        heading_title : 'Ecommerce System',
+        heading_subtitle : 'Full-stack MERN App and Rails CMS/API',
+        background_image : Counterpoint,
+        image_orientation: 'portrait',
         video_link : '',
         live_link : '',
-        repo_link: 'https://github.com/sds-smith/discover-breweries/tree/main',
-        readme_link : 'https://github.com/sds-smith/discover-breweries/blob/main/README.md',
-        description : `Served from AWS S3 via Cloudfront distribution. Lambda functions, triggered by AWS API Gateway and Cloudwatch Events, connect to MongoDB Atlas to perform full range of CRUD operations. Frontend code changes are deployed to S3 via aws cli script`
-    },       
+        repo_link : 'https://github.com/sds-smith/restaurant_menu',
+        readme_link : 'https://github.com/sds-smith/restaurant_menu/blob/main/README.md',
+        description : 'A decoupled eCommerce system consisting of: An ordering app with React frontend, and a menu data API/ CMS tool running on a Ruby on Rails server.'
+    },      
     {   
         id: 'atj-card',
         name : 'Assemble the Jams',
@@ -43,4 +32,30 @@ export const projects = [
         readme_link : 'https://github.com/sds-smith/assemble_the_jams_3/blob/master/README.md',
         description : 'A Spotify playlist generator. React Frontend with GraphQL API on Node/Express server. Authentication and authorization via Passport-Spotify.'
     },
+    {   
+        id: 'discover-breweries-card',
+        name: 'Discover Breweries',
+        action: 'view on github',
+        heading_title : 'Brewery Locator',
+        heading_subtitle : 'React static frontend with AWS Lambda',
+        background_image : DiscoverBreweries,
+        video_link : '',
+        live_link : '',
+        repo_link: 'https://github.com/sds-smith/discover-breweries/tree/main',
+        readme_link : 'https://github.com/sds-smith/discover-breweries/blob/main/README.md',
+        description : `Served from AWS S3 via Cloudfront distribution. Lambda functions, triggered by AWS API Gateway and Cloudwatch Events, connect to MongoDB Atlas to perform full range of CRUD operations. Frontend code changes are deployed to S3 via aws cli script`
+    },   
+    {   
+        id: '1on1boss-card',
+        name: '1on1Boss',
+        action: 'learn more',
+        heading_title : 'Kanban-style Discussion Board',
+        heading_subtitle : 'Full-stack MERN App',
+        background_image : Boss,
+        video_link : '',
+        live_link : 'http://app.1on1boss.com/',
+        repo_link : 'https://www.1on1boss.com/the-team',
+        readme_link : 'https://www.1on1boss.com',
+        description : 'An MVP for a kanban-style discussion management board. Built on the MERN tech stack, runs in Docker container.'
+    }, 
 ]
